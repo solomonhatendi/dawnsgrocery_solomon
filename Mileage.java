0fde3adc2cd1e69d48900935c02772f313132887
Solomon <solomonc19@gmail.com>
2022-11-15 14:16:58 -0500

    Done through user interface.

--- Mileage.java
+++ Mileage.java
@@ -1,1 +1,21 @@
-//File
+import java.util.Scanner;
+
+public classs Mileage
+{
+  /** Testing 1, 2, 3*/
+  public static void main (String[] args)
+  {
+    int miles;
+    double gallons, mpg;
+    //mpg = miles / gallons;
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Enter miles and gallons: ");
+    miles = s.nextInt();
+    gallons = s.nextDouble();
+    mpg = miles / gallons;
+
+    //output
+    System.out.println("Miles" + "Per" + "Gallon: " + mpg);
+  }
+}
